8a47a965412d56f3631e5a75f2f2bbbfbd9d3b6c
Aayush181509 <aayush.181509@ncit.edu.np>
2023-01-02 21:32:59 +0545

    Node Basic Server

---
+++ app.js
@@ -0,0 +1,22 @@
+// Core modules 
+// http https fs path os 
+// Creating server
+
+// const http = require("http");
+
+// const server = http.createServer((req,res)=>{
+//     console.log(req);
+// });
+
+// server.listen(3000);//port number default or port 8
+
+// let x=10;
+// console.log(x);
+
+// let x=15;
+// console.log(x);
+let x=10
+console.log(x) 
+ 
+x=20
+console.log(x)
